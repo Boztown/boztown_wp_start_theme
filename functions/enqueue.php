8d94c4cc24af7a04b5448545bc1b286f72d32b0a
Ryan Bosinger <rbosinger@gmail.com>
2014-12-12 17:48:06 -0700

    Linked up Skeleton framework in enqueue (but commented it out).

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -8,6 +8,8 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 
 function theme_styles() {
+    //wp_enqueue_style( 'skeleton-normalize', get_stylesheet_directory_uri() . '/lib/skeleton-2.0.1/css/normalize.css' );
+    //wp_enqueue_style( 'skeleton', get_stylesheet_directory_uri() . '/lib/skeleton-2.0.1/css/skeleton.css' );
     wp_enqueue_style( 'main', get_stylesheet_uri() );
 }
 
