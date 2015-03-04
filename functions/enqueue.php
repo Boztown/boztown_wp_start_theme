d264f495efa095233705bc02c869d9ccae435a39
Ryan Bosinger <rbosinger@gmail.com>
2015-03-04 10:32:22 -0700

    Ok fine.  Decided to commit a copy of Bootstrap that is only the grid with zero padding.

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -1,6 +1,7 @@
 <?php 
 
 function theme_scripts() {
+    wp_enqueue_script( 'fastclick', get_template_directory_uri() . '/bower_components/fastclick/lib/fastclick.js', array(), '1.0.0', true );
     wp_enqueue_script( 'theme-general', get_template_directory_uri() . '/js/general.js', array('jquery'), '1.0.0', true );
 }
 
@@ -8,8 +9,8 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 
 function theme_styles() {
-    wp_enqueue_style( 'normalize', get_stylesheet_directory_uri() . '/bower_components/normalize.css/normalize.css' );
-    wp_enqueue_style( 'skeleton', get_stylesheet_directory_uri() . '/bower_components/skeleton/css/skeleton.css' );
+    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/lib/bootstrap/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap-theme', get_stylesheet_directory_uri() . '/lib/bootstrap/css/bootstrap-theme.min.css' );
     wp_enqueue_style( 'main', get_stylesheet_uri() );
 }
 
