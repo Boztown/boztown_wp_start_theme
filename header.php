097e7a0e733648cbd963473bbd44272622d7c033
Ryan Bosinger <rbosinger@gmail.com>
2015-03-04 13:24:11 -0700

    Bootstrap  nav walker.

--- header.php
+++ header.php
@@ -8,10 +8,12 @@
 <body <?php body_class( $class ); ?>>
 
 <header class="primary">
-  <nav class="primary">
+  <div class="container">
     <a href="<?php echo home_url(); ?>">
       <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" class="logo" alt="<?php wp_title(); ?>">
     </a>
-    <?php wp_nav_menu(); ?>
-  </nav>
+    <nav class="primary">
+      <?php wp_nav_menu(); ?>
+    </nav>
+  </div>
 </header>
