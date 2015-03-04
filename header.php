e128e4d59ec1949ece8e874d18cd3030da0d139d
Ryan Bosinger <rbosinger@gmail.com>
2015-03-04 11:07:39 -0700

    Header stuff.

--- header.php
+++ header.php
@@ -6,3 +6,12 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class( $class ); ?>>
+
+<header class="primary">
+  <nav class="primary">
+    <a href="<?php echo home_url(); ?>">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" class="logo" alt="<?php wp_title(); ?>">
+    </a>
+    <?php wp_nav_menu(); ?>
+  </nav>
+</header>
