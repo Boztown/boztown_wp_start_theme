bcbc390d04d281232694dbadb04299fd07ae5182
Ryan Bosinger <rbosinger@gmail.com>
2015-03-04 10:36:43 -0700

    ACF maps script (include).

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
-  <?php print_r( get_theme_mod('themename_theme_options[header_select]') ); ?>
+  <?php //print_r( get_theme_mod('themename_theme_options[header_select]') ); ?>
+  <?php 
+    if(file_exists('footer-acf-maps.php')) {
+      include('footer-acf-maps.php'); 
+    }
+  ?>
   <?php wp_footer(); ?>
 </body>
 </html>
