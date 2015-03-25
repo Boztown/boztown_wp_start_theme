9baae135d3204a03f545eecfb56067afe9a374bb
Ryan Bosinger <rbosinger@gmail.com>
2015-03-25 07:14:56 -0600

    Clickbox.

--- js/general.js
+++ js/general.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function( $ ) {
 
+  // add "clickbox" class to a box that has a link inside of it to make
+  // the whole thing clickable.
+  $(document.body).on('click', '.clickbox', function() {
+    window.location = $(this).find('a').attr('href');
+  });
 
 });
