21e64dd2e845dbb833da7c3a08dbd2153fbb149d
Ryan Bosinger <rbosinger@gmail.com>
2015-05-20 10:50:07 -0600

    Got rid of Skeleton example in page-home.

--- page-home.php
+++ page-home.php
@@ -5,7 +5,6 @@
 
 get_header(); ?>
 
-
 <div class="container">
   
   <h1>Home</h1>
@@ -18,37 +17,6 @@ get_header(); ?>
     <?php endwhile; ?> 
   <?php endif; ?>
 
-  <div class="row">
-    <div class="three columns" style="background: red;">ONE</div>
-    <div class="three columns" style="background: blue;">TWO</div>
-    <div class="three columns" style="background: green;">THREE</div>
-  </div>
-
-  <!-- 
-  <div class="row">
-    <div class="one column">One</div>
-    <div class="eleven columns">Eleven</div>
-  </div>
-
-  <div class="row">
-    <div class="two columns">Two</div>
-    <div class="ten columns">Ten</div>
-  </div>
-
-  <div class="row">
-    <div class="one-third column">1/3</div>
-    <div class="two-thirds column">2/3</div>
-  </div>
-
-  <div class="row">
-    <div class="one-half column">1/2</div>
-    <div class="one-half column">1/2</div>
-  </div>
-  -->
-
 </div>
-
-
-
 
 <?php get_footer(); ?>
