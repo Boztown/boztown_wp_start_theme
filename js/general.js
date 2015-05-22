16ff270104a1f8177bface4d22e9a881e14e6901
Ryan Bosinger <rbosinger@gmail.com>
2015-05-22 10:35:48 -0600

    Allowed "click box" function to use "target" attributes.

--- js/general.js
+++ js/general.js
@@ -3,7 +3,20 @@ jQuery(document).ready(function( $ ) {
   // add "clickbox" class to a box that has a link inside of it to make
   // the whole thing clickable.
   $(document.body).on('click', '.clickbox', function() {
-    window.location = $(this).find('a').attr('href');
+    if ($(this).find('a').attr('target') == '_blank') {
+      window.open($(this).find('a').attr('href'), '_blank');
+    } else {
+      window.location = $(this).find('a').attr('href');
+    }
   });
+
+  var iconElement = document.getElementById('icon');
+  var options = {
+      from: 'fa-bars',
+      to: 'fa-arrow-right',
+      animation: 'rubberBand'
+  };
+
+  iconate(iconElement, options);
 
 });
