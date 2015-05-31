98d7f0e8f8c382964af327be3633ec7343a817ae
Ryan Bosinger <rbosinger@gmail.com>
2015-05-31 15:15:33 -0600

    Better title.

--- header.php
+++ header.php
@@ -4,13 +4,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title(); ?></title>
+  <title><?php echo bloginfo( 'name' ); ?><?php wp_title(); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
 <header class="primary">
   <div class="container">
+    <i id="iconn" class="fa fa-bars fa-lg"></i>
     <a href="<?php echo home_url(); ?>">
       <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" class="logo" alt="<?php wp_title(); ?>">
     </a>
