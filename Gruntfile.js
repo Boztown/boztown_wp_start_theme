99b3dc3e2a0a392a5762b62bd310c0f7b83b8292
Ryan Bosinger <rbosinger@gmail.com>
2015-06-02 12:23:22 -0600

    Hacking out the Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
 
+  var conf = grunt.file.readJSON('conf.json');
+
   grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+
     jshint: {
       files: ['Gruntfile.js', 'js/**/*.js'],
       options: {
@@ -15,12 +28,34 @@ module.exports = function(grunt) {
           'dist/output.min.js': ['js/**/*.js']
         }
       }
+    },
+    watch: {
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['jshint', 'uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+      styles: {
+        files: ['styles/*.scss'],
+        tasks: ['compass'],
+        options: {
+          spawn: false,
+        },        
+      }
     }
+  });
+
+  grunt.registerTask('deploy', 'Deploy site.', function() {
+    grunt.log.writeln(conf.environments.production.address);
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-global-config');
 
   grunt.registerTask('compress', ['uglify']);
   grunt.registerTask('default', ['jshint']);
