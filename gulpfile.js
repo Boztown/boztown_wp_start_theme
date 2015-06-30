354a44dfe35f4bd52e0a8bca6c5dc18b339605e1
Ryan Bosinger <rbosinger@gmail.com>
2015-06-30 16:01:51 -0600

    Added gulp compass watch.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,18 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('default', function() {
-  console.log('GUPLY!');
+gulp.task('compass', function() {
+  gulp.src('./styles/*.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: '',
+      sass: 'styles'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    //gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('styles/*.scss', ['compass']);
 });
