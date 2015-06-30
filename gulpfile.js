307bb7d2b2fb13a01f691d16f547a5f55aadfffb
Ryan Bosinger <rbosinger@gmail.com>
2015-06-30 15:56:28 -0600

    Gulp Compass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   console.log('GUPLY!');
